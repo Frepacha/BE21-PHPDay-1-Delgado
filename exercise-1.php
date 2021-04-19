915d99441950be34b38518766b00aed6200646d7
Freddy <freddy.delgado.porras@gmail.com>
2021-04-19 10:48:22 +0200

    i added exercise 1

---
+++ exercise-1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 1</title>
+    <style>
+    h1{
+        display: flex;
+        justify-content: center;
+    }
+    
+    </style>
+</head>
+<body>
+    <?php 
+    $name = '<h1> Freddy Roland Delgado Porras </h1>';
+    echo ($name);
+    ?>
+    
+    <?php 
+    $name1 = '<h1> Freddy <br> Roland <br> Delgado <br> Porras </h1>';
+    echo ($name1);
+    ?>
+
+    
+</body>
+</html>
