dcc44e3ac89fba4e4b529992bf38ff7a762df01f
Freddy <freddy.delgado.porras@gmail.com>
2021-04-19 11:02:29 +0200

    basic exercises completed

---
+++ basic/exercise-2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Exercise 2</title>
+</head>
+<style>
+h1{
+    display: flex;
+    justify-content: center;
+}
+</style>
+<body>
+    <?php
+    $name = 'Freddy';
+    $age = '36';
+    $jobtittle = 'Full-Stack-Webdeveloper';
+
+    echo ("<h1>Hi, my name is $name, and i am $age, and i work as a $jobtittle</h1>");
+    ?>
+</body>
+</html>
---
+++ basic/exercise-3.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php exercise 3</title>
+    <style>
+    h1{
+    display: flex;
+    justify-content: center;
+    }
+    </style>
+    
+</head>
+<body>
+    <?php
+    $players = array('Mark', 'John', 'Georg', 'Lisa');
+    echo ( "<h1>The third player in the team is $players[2]</h1>");
+    
+    ?>
+</body>
+</html>
