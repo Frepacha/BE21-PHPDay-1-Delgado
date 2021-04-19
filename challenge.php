e3d813a1a645436b075af6ed8f53ff4f86362a8c
Freddy <freddy.delgado.porras@gmail.com>
2021-04-19 12:39:55 +0200

    i added in challenge file a array with cars info

---
+++ challenge.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- boostrap is here -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <title>Php Challenge</title>
+</head>
+<body>
+<?php
+       $cars = array(
+           "Car1" => array
+               (
+               "Branch" => "Lamborgini",
+               "Model" => "Landa-530T",
+               "Price"  => 600000
+               ),
+               "Car2" => array
+               (
+               "Branch" => "Ford",
+               "Model" => "Mustang-1970",
+               "Price"  => 100000
+               ),
+               "Car3" => array
+               (
+               "Branch" => "Bugatti",
+               "Model" => "Chiron",
+               "Price"  => 530000
+               ),
+               "Car4" => array
+               (
+               "Branch" => "Bentley",
+               "Model" => "Continental GT3",
+               "Price"  => 800000
+               )
+           );
+           ?>
+</body>
+</html>
